502d8dc22f10d50650f84041b3df0a1281148882
unknown <saiftony5@gmail.com>
2021-10-29 13:35:32 +0100

    last update

--- Student/models.py
+++ Student/models.py
@@ -1,21 +1,8 @@
 from django.db import models
 
 
-
-
-class StudentModel(models.Model):
-    name = models.CharField(' Name',max_length=200 )
-    age = models.IntegerField('Age')
-    address=models.TextField()
-    email=models.EmailField(default="")
-    pin=models.IntegerField(default=0)
-    mob=models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
 class Weather(models.Model):
-    humidity=models.IntegerField(max_length=50,blank=True);
+    humidity=models.IntegerField(blank=True);
     water=models.IntegerField(default=0,blank=True);
     light=models.IntegerField(default=0,blank=True);
     other=models.CharField(max_length=60,blank=True);
--- Student/urls.py
+++ Student/urls.py
@@ -6,15 +6,9 @@ app_name = 'Student'
 
 urlpatterns = [
     # path('', views.add_student, name='add_student'),
-    path('student_manage', views.student_manage, name='student_manage'),
-    path('student_edit/<int:pk>', views.student_edit, name='student_edit'),
-    path('delete_student/<int:pk>', views.delete_student, name='delete_student'),
-    path('show_graph/', views.show_graph, name='show_graph'),
-    path('weather/', views.weather, name='weather'),
-    path('update/', views.weatherapi, name='weatherapi'),
-    path('hh/', views.show_graphh, name='hh'),
+ 
     path('', views.weathernow, name='weathernow'),
+    path('fetch_sensor_values_ajax/', views.fetch_sensor_values_ajax, name='fetch_sensor_values_ajax'),
 
-    path('fetch_sensor_values_ajax', views.fetch_sensor_values_ajax, name='fetch_sensor_values_ajax'),
 
 ]
